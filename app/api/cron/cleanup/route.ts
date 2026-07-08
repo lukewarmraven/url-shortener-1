f696b813a8524a4cf8165423f93a4e6ceff6ee4b
bacquegou <quintoravenluke@gmail.com>
2026-07-08 18:37:59 +0800

    Console log for tracking cron runs

--- app/api/cron/cleanup/route.ts
+++ app/api/cron/cleanup/route.ts
@@ -4,6 +4,8 @@ import Url from "@/model/model";
 export async function GET() {
   await connectDB();
 
+  console.log("Running cron for cleanup...");
+
   const threeDaysAgo = new Date(Date.now() - 3 * 24 * 60 * 60 * 1000);
   const { deletedCount } = await Url.deleteMany({
     createdAt: { $lt: threeDaysAgo },
